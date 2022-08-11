27e3c8090e8bf31be23836dfee384f2a73b9be11
Agam Madan <agam.madan@incedoinc.com>
2022-08-11 23:02:33 +0530

    Updated Db Creation Script and Added Script for Record Insertion

---
+++ TransportManagmentSystemBackend/PostDeployment/Entries_Role.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Role] (Name, Description, CreatedBy) VALUES ('Admin', 'This is Admin Role', 'UserAdmin');
+INSERT INTO [dbo].[Role] (Name, Description, CreatedBy) VALUES ('User', 'This is User Role', 'UserAdmin');
